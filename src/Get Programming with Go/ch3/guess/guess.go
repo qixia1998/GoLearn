efec8891cec7dbfcc7ab2ecaa53973112e6725f7
qixia1998 <qixia17138552@163.com>
2020-05-27 21:03:48 +0800

    updated ch03

---
+++ src/Get Programming with Go/ch3/guess/guess.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var number = 42
+	for {
+		var n = rand.Intn(100) + 1
+		if n < number {
+			fmt.Printf("%v is too samll.\n", n)
+		} else if n > number {
+			fmt.Printf("%v is too big.\n", n)
+		} else {
+			fmt.Printf("You got it! %v\n", n)
+			break
+		}
+	}
+}
