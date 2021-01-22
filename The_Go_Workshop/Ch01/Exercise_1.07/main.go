a1c8fd9a0078adea7f062d464d536bce6a26e3db
qixia1998 <qixia717138552@gmail.com>
2021-01-22 10:39:07 +0800

    Updated Ch01

--- The_Go_Workshop/Ch01/Exercise_1.07/main.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-var defaultOffset = 10
-func main() {
-	offset := defaultOffset
-	fmt.Println(offset)
-	offset = offset + defaultOffset
-	fmt.Println(offset)
-}
--- The_Go_Workshop/Ch01/Exercise_1.08/main.go
+++
@@ -1,7 +0,0 @@
-package main
-import "fmt"
-func main() {
-	query, limit, offset := "bat", 10, 0
-	query, limit, offset = "ball", offset, 20
-	fmt.Println(query, limit, offset)
-}
