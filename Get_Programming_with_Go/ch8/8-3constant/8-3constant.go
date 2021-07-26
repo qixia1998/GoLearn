4faa21e9f1247563f5de725cdfaf9aeb7ddc3b26
qixia1998 <qixia17138552@163.com>
2021-07-26 23:20:56 +0800

    updated ch8

---
+++ Get_Programming_with_Go/ch8/8-3constant/8-3constant.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Andromeda Galaxy is", 24000000000000000000/299792/86400, "light days away.")
+}
---
+++ Get_Programming_with_Go/ch8/8-4constant/8-4constant.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const distance = 24000000000000000000
+	const lightSpeed = 299792
+	const secondsPerDay = 86400
+
+	const days = distance / lightSpeed / secondsPerDay
+	fmt.Println("Andromeda Galaxy is", days, "light days away.")
+
+}
