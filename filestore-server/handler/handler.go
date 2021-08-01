16e07f2c35dd973b0fe0ae7273c4d76be3d00a98
qixia1998 <qixia17138552@163.com>
2021-08-01 22:43:13 +0800

    updated filestore-server

---
+++ filestore-server/handler/handler.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"filestore-server/meta"
+	"filestore-server/util"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+// 处理文件上传
+func UploadHandler(w http.ResponseWriter, r*http.Request){
+	if r.Method == "GET"{
+		// 返回上传html页面
+		data, err := ioutil.ReadFile("./static/view/index.html")
+		if err != nil {
+			io.WriteString(w, "internel server error")
+			return
+		}
+		io.WriteString(w, string(data))
+	}else if r.Method == "POST"{
+		// 接收文件流及存储到本地目录
+		file, head, err := r.FormFile("file")
+		if err != nil{
+			fmt.Printf("Failed to get data, err:%s\n", err.Error())
+			return
+		}
+		defer file.Close()
+
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "C:/Users/qixia/Desktop" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+
+		newFile, err := os.Create(fileMeta.Location)
+		if err != nil{
+			fmt.Printf("Failed to create file, err:%s\n", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		fileMeta.FileSize, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("Failed to save data into file, err:%s\n", err.Error())
+			return
+		}
+
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
+
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
+	}
+}
+
+// UploadSucHandler : 上传已完成
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload finished!")
+}
