50c5d2bac3e518c05c13a4f46eebcc957cf892e9
qixia1998 <qixia17138552@163.com>
2021-09-03 16:15:39 +0800

    updated

--- MasteringGo/ch01/customLogLineNumber.go
+++ MasteringGo/ch01/customLogLineNumber.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+)
+
 func main() {
-	$END$
+	LOGFILE := path.Join(os.TempDir(), "mGo.log")
+	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	lstdFlags := log.Ldate | log.Lshortfile
+	iLog := log.New(f, "LNum", lstdFlags)
+	iLog.Println("Mastering Go, 3rd edition!")
+
+	iLog.SetFlags(log.Lshortfile | log.LstdFlags)
+	iLog.Println("Another log entry!")
 }
