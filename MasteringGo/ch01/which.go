a17839b3fb9f126fb219c6191d1f7af5c19a8e68
qixia1998 <qixia17138552@163.com>
2021-09-03 15:19:23 +0800

    updated

---
+++ MasteringGo/ch01/which.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide an argument!")
+		return
+	}
+	file := arguments[1]
+
+	path := os.Getenv("PATH")
+	pathSplit := filepath.SplitList(path)
+	for _, directory := range pathSplit {
+		fullPath := filepath.Join(directory, file)
+		fileInfo, err := os.Stat(fullPath)
+		if err == nil {
+			mode := fileInfo.Mode()
+			if mode.IsRegular() {
+				if mode&0111 != 0 {
+					fmt.Println(fullPath)
+					return
+				}
+			}
+		}
+	}
+}
