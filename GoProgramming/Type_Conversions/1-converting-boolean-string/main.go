f7e96a51b1a4b68c75ea6ca0a5deec9506fa0349
qixia1998 <qixia17138552@163.com>
2021-09-08 16:24:37 +0800

    updated

---
+++ GoProgramming/Type_Conversions/1-converting-boolean-string/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	isNew := true
+	//isNewStr := strconv.FormatBool(isNew)
+	message := "Purchased item is "
+	fmt.Printf("%s %v", message, isNew)
+}
---
+++ GoProgramming/Type_Conversions/2-converting-numbers-to-string/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	number := 100
+	numberStr := strconv.Itoa(number)
+	fmt.Println(numberStr)
+
+	numberFloat := 23445221.1223356
+	numberFloatStr := strconv.FormatFloat(numberFloat, 'f', -1, 64)
+	fmt.Println(numberFloatStr)
+}
