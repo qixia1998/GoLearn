596108d0d0dc53253df140a72300c0cd8003d20b
qixia1998 <qixia17138552@163.com>
2021-09-09 14:21:15 +0800

    updated ch03

---
+++ MasteringGo/ch03/sliceStruct.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type record struct {
+	Field1 int
+	Field2 string
+}
+
+func main() {
+	S := []record{}
+	for i := 0; i < 10; i++ {
+		text := "text" + strconv.Itoa(i)
+		temp := record{Field1: i, Field2: text}
+		S = append(S, temp)
+	}
+
+	// 访问第一个元素的字段
+	fmt.Println("Index 0:", S[0].Field1, S[0].Field2)
+	fmt.Println("Number of structures:", len(S))
+	sum := 0
+	for _, k := range S {
+		sum += k.Field1
+	}
+	fmt.Println("Sum:", sum)
+}
