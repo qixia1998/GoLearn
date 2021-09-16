d18043a046abe4978be68cf001000e83a5f45ae2
qixia1998 <qixia17138552@163.com>
2021-09-16 15:09:47 +0800

    updated

---
+++ save_the_world_with_go/02_the_basics/init_functions/example_02/go.mod
@@ -0,0 +1,5 @@
+module GoLearn/save_the_world_with_go/02_the_basics/init_functions/example_02/
+
+go 1.17
+
+require github.com/juanmanuel-tirado/savetheworldwithgo v0.2.0 // indirect
---
+++ save_the_world_with_go/02_the_basics/init_functions/example_02/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	_ "github.com/juanmanuel-tirado/savetheworldwithgo/02_the_basics/init_functions/example_02/a"
+	"fmt"
+)
+
+func init() {
+	fmt.Println("Init from my program")
+}
+
+func main() {
+	fmt.Println("My program")
+}
