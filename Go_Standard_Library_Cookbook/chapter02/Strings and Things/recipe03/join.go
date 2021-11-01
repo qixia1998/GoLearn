a73a7d5b13669d5a4cf6726a2f0cb18d4584d03f
qixia1998 <qixia17138552@163.com>
2021-11-01 18:10:50 +0800

    updated

---
+++ Go_Standard_Library_Cookbook/chapter02/Strings and Things/recipe03/join.go
@@ -0,0 +1,21 @@
+package main
+
+// 用分隔符连接字符串切片
+import (
+	"fmt"
+	"strings"
+)
+
+const selectBase = "SELECT * FROM user WHERE %s "
+
+var refStringSlice = []string{
+	" FIRST_NAME = 'Jack' ",
+	" INSURANCE_NO = 333444555 ",
+	" EFFECTIVE_FROM = SYSDATA "}
+
+func main() {
+
+	sentence := strings.Join(refStringSlice, "AND")
+	fmt.Printf(selectBase+"\n", sentence)
+	
+}
