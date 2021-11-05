7af4854488d7315ea319d4f5ce054775484ea283
qixia1998 <qixia17138552@163.com>
2021-11-05 16:05:54 +0800

    updated

---
+++ save_the_world_with_go/09_templates/functions/example_01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+const Msg = `
+The fourth musketeer is:
+{{slice . 3}}
+`
+
+func main() {
+	musketeers := []string{"Athos", "Porthos", "Aramis", "D'Artagnan"}
+
+	t := template.Must(template.New("msg").Parse(Msg))
+
+	err := t.Execute(os.Stdout, musketeers)
+	if err != nil {
+		panic(err)
+	}
+}
