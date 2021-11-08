0daf1b874880fa3634db83dac4f14acb8f39312c
qixia1998 <qixia17138552@163.com>
2021-11-08 14:33:18 +0800

    updated

--- save_the_world_with_go/11_modules/godoc/example_01/example01_test.go
+++ save_the_world_with_go/11_modules/godoc/example_01/example01_test.go
@@ -1,17 +1,17 @@
 package example_01_test
 
-import "github.com/qixia1998/GoLearn/save_the_world_with_go/11_modules/godoc/example_01"
+import "GoLearn/save_the_world_with_go/11_modules/godoc/example_01"
 
 func ExampleMsg_Send() {
-    m := example_01.Msg{"Hello"}
-    m.Send("John")
-    // Output:
-    // Send Hello to John
+	m := example_01.Msg{"Hello"}
+	m.Send("John")
+	// Output:
+	// Send Hello to John
 }
 
 func ExampleMsg_Receive() {
-    m := example_01.Msg{"Hello"}
-    m.Receive("John")
-    // Output:
-    // Received Hello from John
+	m := example_01.Msg{"Hello"}
+	m.Receive("John")
+	// Output:
+	// Received Hello from John
 }
