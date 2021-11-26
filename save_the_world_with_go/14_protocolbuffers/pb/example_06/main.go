5ee0f8f6a229a5e6fe7757d92b465af18bb2c0ee
qixia1998 <qixia17138552@163.com>
2021-11-26 14:15:49 +0800

    updated

---
+++ save_the_world_with_go/14_protocolbuffers/pb/example_06/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"GoLearn/save_the_world_with_go/14_protocolbuffers/pb/example_06/user"
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	goDeveloper := user.Developer{Language: "go"}
+	userA := user.User{UserId: "John", Email: "john@gmail.com",
+		Type: &user.User_Developer{&goDeveloper}}
+	aksOperator := user.Operator{Platform: "aks"}
+	userB := user.User{UserId: "Mary", Email: "mary@gmail.com",
+		Type: &user.User_Operator{&aksOperator}}
+
+	encodedA, err := proto.Marshal(&userA)
+	if err != nil {
+		panic(err)
+	}
+	encodedB, err := proto.Marshal(&userB)
+	if err != nil {
+		panic(err)
+	}
+
+	recoveredA, recoveredB := user.User{}, user.User{}
+	err = proto.Unmarshal(encodedA, &recoveredA)
+	if err != nil {
+		panic(err)
+	}
+	err = proto.Unmarshal(encodedB, &recoveredB)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("RecoveredA:", recoveredA.String())
+	fmt.Println("RecoveredB:", recoveredB.String())
+}
