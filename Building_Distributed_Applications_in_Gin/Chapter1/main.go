8a1d8242cbe42dee47a9c03fd7f8221d28a0f3b5
qixia1998 <qixia717138552@gmail.com>
2021-12-08 11:27:39 +0800

    updated

--- Building_Distributed_Applications_in_Gin/Chapter1/main.go
+++ Building_Distributed_Applications_in_Gin/Chapter1/main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func IndexHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello world",
+	})
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
-	router.Run()
+	router.GET("/", IndexHandler)
+	http.ListenAndServe(":8080", router)
 }
---
+++ Building_Distributed_Applications_in_Gin/Chapter2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "time"
+
+type Recipe struct {
+	Name string `json:"name"`
+	Tags []string `json:"tags"`
+	Ingredients []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	PublishedAt time.Time `json:"publishedAt"`
+}
