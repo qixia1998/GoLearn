f7c9eedfe53c8ab66cf55e8dc3bbe3a8630d29b6
qixia1998 <qixia717138552@gmail.com>
2022-01-19 16:04:35 +0800

    updated

---
+++ Go_Consurrency/lesson_1/channel/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	ch := make(chan struct{}) // 发送和接收count++ “信号”的通道
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 100000; j++ {
+				ch <- struct{}{}
+			}
+		}()
+	}
+
+	go func ()  {
+		wg.Wait() // 等待上面所有的 goroutine 运行完成
+		close(ch) // 关闭通道
+	}()
+
+	count := 0
+	for range ch { // 如果ch通道读取完了（ch是关闭状态），则for循环结束
+		count++
+	}
+	fmt.Println("count的值是:", count)
+}
