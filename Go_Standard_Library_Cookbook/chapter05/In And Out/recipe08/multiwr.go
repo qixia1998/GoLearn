326fe6597a762e3e3ad4eb10789f67ca00cb104b
qixia1998 <qixia717138552@gmail.com>
2022-02-23 10:08:17 +0800

    updated

--- Go_Standard_Library_Cookbook/chapter05/In And Out/recipe08/multiwr.go
+++ Go_Standard_Library_Cookbook/chapter05/In And Out/recipe08/multiwr.go
@@ -1,1 +1,26 @@
-package recipe08
+package main
+
+// 同时写入多个写操作
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+
+	buf := bytes.NewBuffer([]byte{})
+	f, err := os.OpenFile("sample.txt", os.O_CREATE|os.O_RDWR, os.ModePerm)
+
+	if err != nil {
+		panic(err)
+	}
+	wr := io.MultiWriter(buf, f)
+	_, err = io.WriteString(wr, "Hello, Go is awesome!")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Content of buffer: " + buf.String())
+}
--- Go_Standard_Library_Cookbook/chapter05/In And Out/recipe09/pipe.go
+++ Go_Standard_Library_Cookbook/chapter05/In And Out/recipe09/pipe.go
@@ -1,1 +1,28 @@
-package recipe09
+package main
+
+// 读和写之间的管道
+import (
+	"io"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	pReader, pWriter := io.Pipe()
+
+	cmd := exec.Command("echo", "Hello Go!\nThis is example")
+	cmd.Stdout = pWriter
+
+	go func() {
+		defer pReader.Close()
+		if _, err := io.Copy(os.Stdout, pReader); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
