f2e8b692573216993930b6e06b4b516abfbd2c0e
qixia1998 <qixia717138552@gmail.com>
2022-04-18 12:57:20 +0800

    updated

---
+++ save_the_world_with_go/17_cli/cobra/commands/example_01/main.go
@@ -0,0 +1,40 @@
+package main
+
+// 带多个命令的CLI
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var RootCmd = &cobra.Command{
+	Use:  "say",
+	Long: "Root command",
+}
+
+var HelloCmd = &cobra.Command{
+	Use:   "hello",
+	Short: "Say hello",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello!!!")
+	},
+}
+
+var ByeCmd = &cobra.Command{
+	Use:   "bye",
+	Short: "Say goodbye",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Bye!!!")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(HelloCmd, ByeCmd)
+}
+
+func main() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
