0335c4dfa44bb5e373f644d1ad35d856a7d9fa85
qixia1998 <qixia717138552@gmail.com>
2022-06-29 21:31:00 +0800

    updated Interface

---
+++ GoLang_Anvanced/Interfaces.go
@@ -0,0 +1,28 @@
+package main
+
+/* define an interface
+
+
+type interface name interface {
+method_name1 [return_type]
+method_name2 [return_type]
+method_name3 [return_type]
+...
+method_namen [return_type]
+}
+
+// define a struct
+type struct_name struct {
+// variables
+}
+
+// implement interface methods
+func (struct_name_variable struct_name) method_name1() [return_type] {
+// method implementation
+}
+...
+func (struct_name_variable struct_name) method_namen() [return_type] {
+// method implementation
+}
+
+*/
