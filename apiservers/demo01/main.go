50f3cd969843522740f7300769d8ad2d9b80d159
qixia1998 <qixia717138552@gmail.com>
2023-03-08 17:30:53 +0800

    add demo01

---
+++ apiservers/demo01/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"GoLearn/apiservers/demo01/router"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	// Create the Gin engine.
+	g := gin.New()
+
+	// gin middlewares
+	middlewares := []gin.HandlerFunc{}
+
+	// Routes.
+	router.Load(
+		// Cores.
+		g,
+
+		// Middlewares.
+		middlewares...,
+	)
+
+	// Ping the server to make sure the router is working.
+	go func() {
+		if err := pingServer(); err != nil {
+			log.Fatal("The router has no response, or it might took too long to start up.", err)
+		}
+		log.Print("The router has been deployed successfully.")
+	}()
+
+	log.Printf("Start to Listening the incoming requests on http address: %s", ":8080")
+	log.Printf(http.ListenAndServe(":8080", g).Error())
+}
+
+// pingServer pings the http server to make sure the router is working.
+func pingServer() error {
+	for i := 0; i < 2; i++ {
+		// Ping the server by sending a GET request to `/health`.
+		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
+		if err == nil && resp.StatusCode == 200 {
+			return nil
+		}
+
+		// Sleep for a second to continue the next ping.
+		log.Print("Waiting for the router, retry in 1 second.")
+		time.Sleep(time.Second)
+	}
+	return errors.New("Cannot connect to the router.")
+}
