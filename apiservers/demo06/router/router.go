8447fb52e2e689de26afc22e5b8c35d380500dfa
qixia1998 <qixia717138552@gmail.com>
2023-03-22 23:55:07 +0800

    add demo06

---
+++ apiservers/demo06/router/router.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"GoLearn/apiservers/demo06/handler/sd"
+	"GoLearn/apiservers/demo06/handler/user"
+	"GoLearn/apiservers/demo06/router/middleware"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// Load loads the middlewares, routers, handlers.
+func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
+	// Middlewares.
+	g.Use(gin.Recovery())
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Options)
+	g.Use(middleware.Secure)
+	g.Use(mw...)
+	// 404 Handler
+	g.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "The incorrect API route.")
+	})
+
+	u := g.Group("/v1/user")
+	{
+		u.POST("", user.Create)
+	}
+
+	// The health check handlers
+	svcd := g.Group("/sd")
+	{
+		svcd.GET("/health", sd.HealthCheck)
+		svcd.GET("/disk", sd.DiskCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
+		svcd.GET("/ram", sd.RAMCheck)
+	}
+
+	return g
+}
