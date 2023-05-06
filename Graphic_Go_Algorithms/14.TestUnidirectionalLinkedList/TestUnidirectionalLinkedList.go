dc704d959dc94113abb1e3148964f7b4dd46a0b6
qixia1998 <qixia717138552@gmail.com>
2023-05-06 23:51:54 +0800

    add UnidirectionalLinkedList

--- Graphic_Go_Algorithms/14.TestUnidirectionalLinkedList/TestUnidirectionalLinkedList.go
+++ Graphic_Go_Algorithms/14.TestUnidirectionalLinkedList/TestUnidirectionalLinkedList.go
@@ -14,17 +14,10 @@ func initial() {
 	head.data = "San Francisco"
 	head.next = nil
 
-	var nodeOakland *Node = &Node{
-		data: "Oakland",
-		next: nil,
-	}
+	var nodeOakland *Node = &Node{data: "Oakland", next: nil}
 	head.next = nodeOakland
-
-	var nodeBerkeley *Node = &Node{
-		data: "Oakland",
-		next: nil,
-	}
-	head.next = nodeBerkeley
+	var nodeBerkeley *Node = &Node{data: "Berkeley", next: nil}
+	nodeOakland.next = nodeBerkeley
 
 	//var tail *Node = &Node{
 	//	data: "Fremont",
@@ -33,6 +26,23 @@ func initial() {
 	tail.data = "Fremont"
 	tail.next = nil
 	nodeBerkeley.next = tail
+}
+
+func insert(insertPosition int, data string) {
+	var p = head
+	var i = 0
+	// Move the node to the insertion position
+	for {
+		if p.next == nil || i >= insertPosition-1 {
+			break
+		}
+		p = p.next
+		i++
+	}
+	var newNode *Node = new(Node)
+	newNode.data = data
+	newNode.next = p.next // newNode next point to next node
+	p.next = newNode      // current next point to newNode
 }
 
 func add(data string) {
@@ -56,8 +66,11 @@ func output(node *Node) {
 func main() {
 	initial()
 
-	fmt.Printf("Append a new node name: Walnut to the end: \n")
-	add("Walnut")
+	//fmt.Printf("Append a new node name: Walnut to the end: \n")
+	//add("Walnut")
+
+	fmt.Printf("Insert a new node Walnut at index = 2: \n")
+	insert(2, "Walnut")
 
 	output(head)
 }
