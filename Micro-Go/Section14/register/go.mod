f45fb70d8a289c42f9db6ef798faee47266f1721
qixia1998 <qixia717138552@gmail.com>
2023-05-26 23:46:22 +0800

    add selection14

---
+++ Micro-Go/Section14/register/go.mod
@@ -0,0 +1,28 @@
+module GoLearn/Micro-Go/Section14/register
+
+go 1.18
+
+require (
+	github.com/go-kit/kit v0.12.0
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/mux v1.8.0
+)
+
+require (
+	github.com/armon/go-metrics v0.3.9 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/hashicorp/consul/api v1.20.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v0.16.2 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+)
---
+++ Micro-Go/Section14/register/service/service.go
@@ -0,0 +1,44 @@
+package transport
+
+import (
+	"GoLearn/Micro-Go/Section14/register/discovery"
+	"context"
+	"errors"
+	"github.com/hashicorp/consul/api"
+	"log"
+)
+
+type Service interface {
+	HealthCheck() string
+
+	DiscoveryService(ctx context.Context, serviceName string) ([]*api.AgentService, error)
+}
+
+var ErrNotServiceInstances = errors.New("instances are not existed")
+
+type RegisterServiceImpl struct {
+	discoveryClient *discovery.DiscoveryClient
+}
+
+func NewRegisterServiceImpl(discoveryClient *discovery.DiscoveryClient) Service {
+	return &RegisterServiceImpl{
+		discoveryClient: discoveryClient,
+	}
+}
+
+func (service *RegisterServiceImpl) DiscoveryService(ctx context.Context, serviceName string) ([]*api.AgentService, error) {
+
+	instances, err := service.discoveryClient.DiscoverServices(ctx, serviceName)
+
+	if err != nil {
+		log.Printf("get service info err: %s", err)
+	}
+	if instances == nil || len(instances) == 0 {
+		return nil, ErrNotServiceInstances
+	}
+	return instances, nil
+}
+
+func (*RegisterServiceImpl) HealthCheck() string {
+	return "OK"
+}
